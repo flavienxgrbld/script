7846d9d5e3d84820346ab3956323ae21ced7a0aa
flavienxgrbld <flavien.garibaldi@gmail.com>
2024-11-08 17:28:34 +0100

    ascii anonymous

--- script.bat
+++ script.bat
@@ -1,57 +1,60 @@
 @echo off
 color 0a
-echo                                     .:-=+#%@@@@@@@@@@@@%#*=--:                                     
-echo                               .:=*@@@@#*+-:...        .:-+*%%@@@#=:.                               
-echo                             :*@@@#=:                            .=#@@@#-.                           
-echo                         .=%@@*:                                      .=@@%+:                        
-echo                      .+@@*-.                                            .-*@@*.                     
-echo                    -#@%=.                                                  .-#@%=.                  
-echo                  -%@*:                                                        .+@%=.                
-echo               .-%@*.                                                            .=%@+.              
-echo              :#@+.                                                                 =%@=             
-echo            .*@#.                                                                    :+@#.           
-echo           -@@-         :+-.            ....--::-:-::--:::::..             :=-         :#@=          
-echo          *@*       .=#@#:          .:::..:-.  :: :: .:. .::..:::.          :#@%+:       +@#.        
-echo        .%@=    .: =@@@+.        .::.   ::.   ::  ::  .-   .::.  .::.         =@@@+:..    -@%:       
-echo       :%@:   :*#-*@@%=       ..::    :-.    ::   ::   .:     ::    .::.       -%@@%-+*-   :#@-      
-echo      .%@:   =@#.+@@+=-     .::..::..::     .:.   .:    :.     .:.:::...:.     :-+@@#:*@*.  :#@-     
-echo     .@@: . =@@--@+:=#+.   ::.     .::::::. ::   .-=:.  ....::::.:.      ::.   -#+.*@-:%@*.  .%@-    
-echo     %@- .=:@@%:-:=%@+   .:.       :.      .::.-%#--@@@*.:.      .:.      .:.   :%@=:=.*@@--: .@%:   
-echo    *@= .@*:@@=:#@@%:   .:        ::       ::  +@@= -@@@ ::       .:.       .:.  :%@@%:=@@--@: .@%  .
-echo   :@#  *@-:@#*@@#=    ::        ::        :.   :. :%@%- .:        ..        .:.   :*@@*#@=.@@. *@=  
-echo   %@: -@@+.@@@*:==.  ::        .:.        :      :#*-.   :.        :.        ::   -=-=%@@:-@@+ .%@. 
-echo  =@*  +@@*:@*:=%%:  ::.::::.   :.        .-      -+.     ::        .:  ..::::.::  .*%+:+@:+@@#  =@* 
-echo  #@: .+@@%:=-#@@-  .:.     ..::::....    ..      :-      ::    ....:-:::.     ..:  :%@@=+.*@@%. .@@.
-echo .@%  .-@@%.=@@@=   :.         ..   ...::.--::::.=@@*:::::--::::. .  .:         .:   -%@@#.*@@+.. *@-
-echo +@= ==.@@*+@@*-   .:          :.         :.     .+*:     .:          :          ..   .*@@**@@.== :@*
-echo %@. %# =@%@@=.=-  :.          :.         :.      ...      :          :.         .:  .+:-@@%@*.#@. @@
-echo @@. @@..%@@= *%.  :.         .:          :.    .-===:..   -          :.         .:.  ##::%@@: @@. @@
-echo @@  *@#.:@-.#@*   :.         .:          :.-#-:*#@@%*-.*-.:          :.          ..  +@%::%- *@@. %@
-echo @@  +@@*.=.#@@=   ::::::::::.:-:::......:+#@@:  :@@=   @@%*-...:.::::-:::::::::::-.  :%@%--.=@@*  #@
-echo @@  :%@@*.*@@#    :.         .. .:--=+*%@@@@+  --@@=:  -@@@@%#*=--:....          :.   *@@#.-@@@=  #@
-echo @@   +@@@-%@@-..  :.         ..+@@@@@@@@@@@@-    %%     @@@@@@@@@@@@+:.          :.  ..#@@-%@@*.  #@
-echo %@. *.-@@*%@*.*=  :.          .#@@@@@@@@@@@@- . =@@+    @@@@@@@@@@@@%-          .:. =#:=@@#@@# #. @@
-echo *@- %+ -@@@%::@%  .:          :%@@@@@@@@@@@@+   +@@#   :@@@@@@@@@@@@@=          .:  +@- %@@@+ =@.:@#
-echo .@% +@*..#@% =@%.  :.         -@@@@@@@@@@@@@%-  *@@%   %@@@@@@@@@@@@@+         .:.  #@*.#@%: *@# *@=
-echo  %@  %@@-.+#.*@@-  .:       ..*@@@@@@@@@@@@@@#. #@@@  *@@@@@@@@@@@@@@#:.       :.  :%@@:##.-%@@: @@.
-echo  -@+ :%@@#=:.%@@=.. ....:::...*@@@@@@@@@@@@@@@# #@@% +@@@@@@@@@@@@@@@#:.::::..:: ..-%@@-:-#@@@- -@* 
-echo   @@. :*@@@*:+@@=:#- :-.     .#@@@@@@@@@@@@@@@@#%@@@*@@@@@@@@@@@@@@@@%:      ::.-*--@@#-=@@@#:  %@: 
-echo   -@#  :=@@@#+%@#.*%- :.     -%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+     .: :%%:=@@++@@@+:  *@+  
-echo    #@= -+ =@@@#@%.-@@- ::    =@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*    .:.:#@= *@#@@@=.== :@%   
-echo     @@: #%: .*@@@--@@%: ::.  +@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#   :: .#@@-:#@@*- .#%. %@- . 
-echo     .%%:.*@@+: :+#-*@@*......*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#:::.  +@@*-**- :=#@%. #@-    
-echo      -%@: -%@@%*=: -@@@+=*- -%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=.:*+=%@@=..-*%@@@+..*@-     
-echo       -%%: .=%@@@@%==#@#-+@*-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%=+@*:*@%==#@@@@%+: .%@-      
-echo        :@@=   -+@@@@@%%@#:+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*.*@%%@@@@@*-.  :%@-       
-echo          #@*  -+  -*%@@@@#-=@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=:*@@@@@#- :=-  =@%:        
-echo           +@%: :%%#=.       .=%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*         -*@%-  *@*.         
-echo            :#@*. .+%@@@@@@@@@@@%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%%@@@@@@@@@@*:  =@%-           
-echo              =%%=.  .-+#@@@@@@@#+-:.-%@@@@@@@@@@@@@@@@@@@@@@@@@=::-+*%@@@@@@#+-:.  -%@+.            
-echo               .+@%=  .-=:      .-+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%+-.      -=-.  -#@*.              
-echo                 .=@@+. :+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*-..=%@*:                
-echo                    =%@*:  :=*%%%##+=#@@@@@@@@@@@@@@@@@@@@@@@@@@%++#%%%%#=:  :+@@+.                  
-echo                      .*@@*:         *@@@@@@@@@@@@@@@@@@@@@@@@@@%.        :*@@*:                     
-echo                         .#@@%=.     #@@@@@@@@@@@@@@@@@@@@@@@@@@@:     :#@@%:                        
-echo                            .=#@@%+--#@@@@@@@@@@@@@@@@@@@@@@@@@@%-.+%@@%=:                           
-echo                                :-*%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*-:.                              
-echo                                     .-=+*#%@@@@@@@@@@@@@@%#*+=-.                                    
+
+
+
+echo                                       uje4QEAAAAAAAAAAAABLXalpw                                     
+echo                                       uje4QEAAAAAAAAAAAABLXalpw                                     
+echo                                 wfSAAAANW8jt            wk8TICAAAQfv                                
+echo                             rXAAAOdx                             cLAAAOn                            
+echo                          dHAAYs                                       cAAH3t                        
+echo                      4AASk                                              pUAAZ                      
+echo                    pOAJh                                                    kNAId                   
+echo                  jJAUs                                                         4ABb                 
+echo                kCAW                                                              aBA0               
+echo              rKA3                                                                  bBAh             
+echo             ZAO                                                                     z7AOz           
+echo           kAAm         q7i                 pmttpuqsskptrqtx               rcp         wJAa          
+echo          VAT        dLAKs          zsrw  rq   rx zs  s   wv  xqw           sOAG7v       8AM         
+echo         HA0     x aAAA3          vu    vu    vx  xr   q    xw    xv          bAAA3v      qACt       
+echo       wFAr   qVPqUAABc         rx    yq     xr   yu   zu     wv     rw        nEAAHm6Tm   wMAl      
+echo      zEAr   0AL 2AA7ho      xv  vu  wx      t     v    u       t xuv   w      um5AAMwWAV   xOAo     
+echo      AAs   gAAlmA3taN8    xt       tvwvxy  yt    kfv       zurtzv       yv    iL7 ZAiuEA1    FAq    
+echo     IAn  ewAAFwowgIA3    uz       t        sx mBKkiAAAZ u        x        w    rHAayazSAAjjx  AEv   
+echo    VAb  AZxAAdsMAABr    t        wy       xt  6AAa lAAA ws        w        zt   uHAAEsgAAmjAt zAC   
+echo   sAJ  TAmyALTAAOg    wu        yv        w    u  vFAHk  t        zz         x    sVAAXMAhzAAz ZAc  
+echo   HAt kAA4zAAAYqhc   wt         t         s      rPTk    t         w         zy   mfo0CAAtmAA5  EA  
+echo eA1  5AAUxAUrgIEw  wx wvvy    wz         o      l4      uz         y    zvux xx   SC9r6As0AAO  eA1 
+echo JAt  6AADyfmMAAj   u        yvrx         z      ui      yx        xqqwz       zz  tCAAd9 SAAF   AA 
+echo AH   lAAH 0AAAf   v                  xwzipuwur 0AAWuuuuypjxtxx      x          w   pEAAOzWAA3   YAn
+echo 6Ah 0d AAT6AAVl    w          w          x       4Xv      v          x          zz    VAAWSAAzfa rAT
+echo DA  FJ hACAAc 0o  x           u          t                s          u           x   8wiAAGAV MA  AA
+echo AA  AA  DAAe 1H   w           w          q      kabgu     p          v           u   MJwxCAAx AA  AA
+echo AA  SAN xAn RAS   y           x          y kRqtXJAAFVk Tk x          y           z   7AFuvCi VAA  IA
+echo AA  9AAT e LAAe   quxtuuvvuw roust      v7LAAu  qAAc   AAFYl   wzzxyvqtuuuuuuuuxwo   yFAFqn dAAS  OA
+echo AA  tBAAY YAAO    x              upmd3SEAAAA1  mpAAct  kAAAABLZgpqu   z          w    ZAAN mAAAe  PA
+echo AA   3AAApDAAp    w            0AAAAAAAAAAAAk    HC     AAAAAAAAAAAA2t           s    zLAAjFAAU   MA
+echo DA  Z mAATCAW T0  u            LAAAAAAAAAAAAn   gAA7    AAAAAAAAAAAAEl           w  eJseAAOAAN M  AA
+echo 1Ak C6 pAAACwsAI   w          vFAAAAAAAAAAAA9   7AAO   rAAAAAAAAAAAAAg           x  3Am IAAA3 aA yAN
+echo  AI 7AS zOAC hAC   u          mAAAAAAAAAAAAABl  WAAF   GAAAAAAAAAAAAA0          w   PA1 NAEt ZAR ZAe
+echo  EA  CAAl 3L SAAj   x         YAAAAAAAAAAAAAAM  QAAA  VAAAAAAAAAAAAAAMx        y   wDAAtPR oCAAw AA 
+echo  jA2 uHAANhw DAAa   zz  xvyz  1AAAAAAAAAAAAAAAO RAAE 7AAAAAAAAAAAAAAAMx zywvz vw   pBAAjvkRAAAi nAX 
+echo   AA  tZAAAWr2AA0sRo wq       OAAAAAAAAAAAAAAAAOBAAAVAAAAAAAAAAAAAAAAFr      xv qVmoAALlcAAAJr  HAw 
+echo   kAN  ueAAAP4DAQ TCn yz     lBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA7      x rGJvdAA72AAA6z  YA7  
+echo    PAd k9 aAAAKAI nAAj xx    bAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAW    zu tJAa UAJAAAd ca qAJ   
+echo     AAx NEr  ZAAAkpAAHw xw   2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO   tx  RAAkzJAAUq  LI  EAp   
+echo      BBx TAA8x x3OlXAAU   z  VAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJwwt   5AAXpZSq yeJAI  OAk    
+echo      qFAy jBAAFYcv jAAA9gVm pIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAc vV3gIAAb  lXCAAA5  SAi     
+echo       lFCz zcJAAAAEdeJAMp3AVjJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEf2ATy1AJcfOAAAAE5x  HAi      
+echo        wAAf   p3AAAAAHCAPy4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATzYACGAAAAASj   sGAj       
+echo          RA1  o7  qUEAAAAOnaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgvXAAAAAJn xck  bAEu        
+echo           8ABw yGDMg         bBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1         qTAHm  WAV          
+echo            yOAY   9DAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAECAAAAAAAAAAYv  dADm           
+echo              dFBf   zn6LAAAAAAAJ3jx iHAAAAAAAAAAAAAAAAAAAAAAAAAdxyl7SEAAAAAAL3it   oIA3             
+echo                9AGa   nfq       m0NAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI4o       qbj   lLAW               
+echo                  0AA3  t2DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUm  aEA1v                
+echo                    eEASr  xeUIGIKO4fLAAAAAAAAAAAAAAAAAAAAAAAAAAC93LHFEHRbx  v1AA6                   
+echo                       YAAWq         YAAAAAAAAAAAAAAAAAAAAAAAAAAC         wYAATu                     
+echo                         zQAAIf      OAAAAAAAAAAAAAAAAAAAAAAAAAAAw     sLAAGu                        
+echo                             hQAAE1plNAAAAAAAAAAAAAAAAAAAAAAAAAAFq 9FAAIct                           
+echo                                xiYHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATix                               
