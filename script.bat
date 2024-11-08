d501c7b3159d2b10d607759750c756047a150b1c
flavienxgrbld <flavien.garibaldi@gmail.com>
2024-11-08 17:42:44 +0100

    idem + timeout

--- script.bat
+++ script.bat
@@ -1,117 +1,115 @@
 @echo off
 color 0a
 
-
-
 echo                                       uje4QEAAAAAAAAAAAABLXalpw                                     
-sleep 1
+timeout /t 1 /nobreak
 echo                                       uje4QEAAAAAAAAAAAABLXalpw                                     
-sleep 1
+timeout /t 1 /nobreak
 echo                                 wfSAAAANW8jt            wk8TICAAAQfv                                
-sleep 1
-echo                             rXAAAOdx                             cLAAAOn                            
-sleep 1
-echo                          dHAAYs                                       cAAH3t                        
-sleep 1
-echo                      4AASk                                              pUAAZ                      
-sleep 1
+timeout /t 1 /nobreak
+echo                             rXAAAOdx                             cLAAAOn                             
+timeout /t 1 /nobreak
+echo                          dHAAYs                                       cAAH3t                         
+timeout /t 1 /nobreak
+echo                      4AASk                                              pUAAZ                       
+timeout /t 1 /nobreak
 echo                    pOAJh                                                    kNAId                   
-sleep 1
+timeout /t 1 /nobreak
 echo                  jJAUs                                                         4ABb                 
-sleep 1
+timeout /t 1 /nobreak
 echo                kCAW                                                              aBA0               
-sleep 1
+timeout /t 1 /nobreak
 echo              rKA3                                                                  bBAh             
-sleep 1
+timeout /t 1 /nobreak
 echo             ZAO                                                                     z7AOz           
-sleep 1
+timeout /t 1 /nobreak
 echo           kAAm         q7i                 pmttpuqsskptrqtx               rcp         wJAa          
-sleep 1
+timeout /t 1 /nobreak
 echo          VAT        dLAKs          zsrw  rq   rx zs  s   wv  xqw           sOAG7v       8AM         
-sleep 1
+timeout /t 1 /nobreak
 echo         HA0     x aAAA3          vu    vu    vx  xr   q    xw    xv          bAAA3v      qACt       
-sleep 1
+timeout /t 1 /nobreak
 echo       wFAr   qVPqUAABc         rx    yq     xr   yu   zu     wv     rw        nEAAHm6Tm   wMAl      
-sleep 1
+timeout /t 1 /nobreak
 echo      zEAr   0AL 2AA7ho      xv  vu  wx      t     v    u       t xuv   w      um5AAMwWAV   xOAo     
-sleep 1
+timeout /t 1 /nobreak
 echo      AAs   gAAlmA3taN8    xt       tvwvxy  yt    kfv       zurtzv       yv    iL7 ZAiuEA1    FAq    
-sleep 1
+timeout /t 1 /nobreak
 echo     IAn  ewAAFwowgIA3    uz       t        sx mBKkiAAAZ u        x        w    rHAayazSAAjjx  AEv   
-sleep 1
+timeout /t 1 /nobreak
 echo    VAb  AZxAAdsMAABr    t        wy       xt  6AAa lAAA ws        w        zt   uHAAEsgAAmjAt zAC   
-sleep 1
+timeout /t 1 /nobreak
 echo   sAJ  TAmyALTAAOg    wu        yv        w    u  vFAHk  t        zz         x    sVAAXMAhzAAz ZAc  
-sleep 1
+timeout /t 1 /nobreak
 echo   HAt kAA4zAAAYqhc   wt         t         s      rPTk    t         w         zy   mfo0CAAtmAA5  EA  
-sleep 1
+timeout /t 1 /nobreak
 echo eA1  5AAUxAUrgIEw  wx wvvy    wz         o      l4      uz         y    zvux xx   SC9r6As0AAO  eA1 
-sleep 1
+timeout /t 1 /nobreak
 echo JAt  6AADyfmMAAj   u        yvrx         z      ui      yx        xqqwz       zz  tCAAd9 SAAF   AA 
-sleep 1
+timeout /t 1 /nobreak
 echo AH   lAAH 0AAAf   v                  xwzipuwur 0AAWuuuuypjxtxx      x          w   pEAAOzWAA3   YAn
-sleep 1
+timeout /t 1 /nobreak
 echo 6Ah 0d AAT6AAVl    w          w          x       4Xv      v          x          zz    VAAWSAAzfa rAT
-sleep 1
+timeout /t 1 /nobreak
 echo DA  FJ hACAAc 0o  x           u          t                s          u           x   8wiAAGAV MA  AA
-sleep 1
+timeout /t 1 /nobreak
 echo AA  AA  DAAe 1H   w           w          q      kabgu     p          v           u   MJwxCAAx AA  AA
-sleep 1
+timeout /t 1 /nobreak
 echo AA  SAN xAn RAS   y           x          y kRqtXJAAFVk Tk x          y           z   7AFuvCi VAA  IA
-sleep 1
+timeout /t 1 /nobreak
 echo AA  9AAT e LAAe   quxtuuvvuw roust      v7LAAu  qAAc   AAFYl   wzzxyvqtuuuuuuuuxwo   yFAFqn dAAS  OA
-sleep 1
+timeout /t 1 /nobreak
 echo AA  tBAAY YAAO    x              upmd3SEAAAA1  mpAAct  kAAAABLZgpqu   z          w    ZAAN mAAAe  PA
-sleep 1
+timeout /t 1 /nobreak
 echo AA   3AAApDAAp    w            0AAAAAAAAAAAAk    HC     AAAAAAAAAAAA2t           s    zLAAjFAAU   MA
-sleep 1
+timeout /t 1 /nobreak
 echo DA  Z mAATCAW T0  u            LAAAAAAAAAAAAn   gAA7    AAAAAAAAAAAAEl           w  eJseAAOAAN M  AA
-sleep 1
+timeout /t 1 /nobreak
 echo 1Ak C6 pAAACwsAI   w          vFAAAAAAAAAAAA9   7AAO   rAAAAAAAAAAAAAg           x  3Am IAAA3 aA yAN
-sleep 1
+timeout /t 1 /nobreak
 echo  AI 7AS zOAC hAC   u          mAAAAAAAAAAAAABl  WAAF   GAAAAAAAAAAAAA0          w   PA1 NAEt ZAR ZAe
-sleep 1
+timeout /t 1 /nobreak
 echo  EA  CAAl 3L SAAj   x         YAAAAAAAAAAAAAAM  QAAA  VAAAAAAAAAAAAAAMx        y   wDAAtPR oCAAw AA 
-sleep 1
+timeout /t 1 /nobreak
 echo  jA2 uHAANhw DAAa   zz  xvyz  1AAAAAAAAAAAAAAAO RAAE 7AAAAAAAAAAAAAAAMx zywvz vw   pBAAjvkRAAAi nAX 
-sleep 1
+timeout /t 1 /nobreak
 echo   AA  tZAAAWr2AA0sRo wq       OAAAAAAAAAAAAAAAAOBAAAVAAAAAAAAAAAAAAAAFr      xv qVmoAALlcAAAJr  HAw 
-sleep 1
+timeout /t 1 /nobreak
 echo   kAN  ueAAAP4DAQ TCn yz     lBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA7      x rGJvdAA72AAA6z  YA7  
-sleep 1
+timeout /t 1 /nobreak
 echo    PAd k9 aAAAKAI nAAj xx    bAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAW    zu tJAa UAJAAAd ca qAJ   
-sleep 1
+timeout /t 1 /nobreak
 echo     AAx NEr  ZAAAkpAAHw xw   2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO   tx  RAAkzJAAUq  LI  EAp   
-sleep 1
+timeout /t 1 /nobreak
 echo      BBx TAA8x x3OlXAAU   z  VAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJwwt   5AAXpZSq yeJAI  OAk    
-sleep 1
+timeout /t 1 /nobreak
 echo      qFAy jBAAFYcv jAAA9gVm pIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAc vV3gIAAb  lXCAAA5  SAi     
-sleep 1
+timeout /t 1 /nobreak
 echo       lFCz zcJAAAAEdeJAMp3AVjJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEf2ATy1AJcfOAAAAE5x  HAi      
-sleep 1
+timeout /t 1 /nobreak
 echo        wAAf   p3AAAAAHCAPy4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATzYACGAAAAASj   sGAj       
-sleep 1
+timeout /t 1 /nobreak
 echo          RA1  o7  qUEAAAAOnaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgvXAAAAAJn xck  bAEu        
-sleep 1
+timeout /t 1 /nobreak
 echo           8ABw yGDMg         bBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1         qTAHm  WAV          
-sleep 1
+timeout /t 1 /nobreak
 echo            yOAY   9DAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAECAAAAAAAAAAYv  dADm           
-sleep 1
+timeout /t 1 /nobreak
 echo              dFBf   zn6LAAAAAAAJ3jx iHAAAAAAAAAAAAAAAAAAAAAAAAAdxyl7SEAAAAAAL3it   oIA3             
-sleep 1
+timeout /t 1 /nobreak
 echo                9AGa   nfq       m0NAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI4o       qbj   lLAW               
-sleep 1
+timeout /t 1 /nobreak
 echo                  0AA3  t2DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUm  aEA1v                
-sleep 1
+timeout /t 1 /nobreak
 echo                    eEASr  xeUIGIKO4fLAAAAAAAAAAAAAAAAAAAAAAAAAAC93LHFEHRbx  v1AA6                   
-sleep 1
+timeout /t 1 /nobreak
 echo                       YAAWq         YAAAAAAAAAAAAAAAAAAAAAAAAAAC         wYAATu                     
-sleep 1
+timeout /t 1 /nobreak
 echo                         zQAAIf      OAAAAAAAAAAAAAAAAAAAAAAAAAAAw     sLAAGu                        
-sleep 1
+timeout /t 1 /nobreak
 echo                             hQAAE1plNAAAAAAAAAAAAAAAAAAAAAAAAAAFq 9FAAIct                           
-sleep 1
+timeout /t 1 /nobreak
 echo                                xiYHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATix                               
-
+timeout /t 1 /nobreak
 
 pause
